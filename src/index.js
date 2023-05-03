57059b5ec1ad02241f024a58580419c9e24d5262
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-03 16:53:19 +0530

    Render Multiple Elements inside ReactDOM.render()

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-//required modules usinfg ES6 syntax
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -6,24 +5,9 @@ import ReactDOM from 'react-dom';
 // kya dikhana hai needs react module
 // kaha dikhana hai needs react-dom module
 
-// ReactDOM.render( <h1>Hello World!!</h1>, document.getElementById('root'));
-
-// Babel is a javascript compiler
-
-// JSX is javascript exts
-
-{/* <h1></h1> */}
-// jsx expression in react
-
-// // React module is imported to use JSX
-
-//     // this is how it is rendered by Babel
-//         import { jsx as _jsx } from "react/jsx-runtime";
-//         ReactDOM.render( /*#__PURE__*/_jsx("h1", {
-//         children: "Hello World!!"
-//         }), document.getElementById('root'));
-
-// to create a html element using pure javascript
-var h1 = document.createElement("h1");
-h1.innerHTMl = "Thapa Technical";
-document.getElementById("root").appendChild(h1);
+ReactDOM.render( <div>
+<h1>Hello World!!</h1>
+<p>Plz like and share</p>
+<h2>plz subscribe</h2>
+</div>
+, document.getElementById('root'));
