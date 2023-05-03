7505ffca224061d7508e63fe194ef5fbcac595b1
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-03 15:53:09 +0530

    importing modules in ES5 syntax, render() method

---
+++ src/index.js
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');        //require modules
+
+// ReactDOM.render('kya dikhana hai', 'kaha dikhana hai', 'callback function');  //render() shows
+// kya dikhana hai needs react module
+// kaha dikhana hai needs react-dom module
+
+ReactDOM.render( <h1>Hello World!!</h1>, document.getElementById('root'));
+
+// Babel is a javascript compiler
