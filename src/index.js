ad94c9ce9a120da1d7241679bc9f68eaf4f1110e
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-04 18:13:37 +0530

    CSS Styling using objects

--- src/index.js
+++ src/index.js
@@ -1,21 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// ReactDOM.render('kya dikhana hai(JSX element)', 'kaha dikhana hai', 'callback function');  //render() shows
-// kya dikhana hai needs react module
-// kaha dikhana hai needs react-dom module
+import './index.css';
+import Heading from './Heading.js';
 
 const fname = "Diksha";
 const lname = "Gowda";
+const bleh = "blah blah blah";
+const img1 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
+const links = "https://picsum.photos";
 
-// Using Template Literals (backticks (`))
-console.log(`my name is ${fname}`);
+// const varname = {
+//     key: 'value',
+//     key: 'value'
+// }
 
-ReactDOM.render( 
+const heading = {
+    color: "#663399",
+    textTransform: "capitalize",
+    textAlign: "center",
+    fontWeight: "bold",
+    textShadow: "0 2px 4px #ffe9c5",
+    margin: "70px 0",
+    fontFamily: '"Josefin Sans", sans-serif'
+}
+ReactDOM.render(
 <>
-<h1><i>My name is {fname + "" + lname}</i></h1>
-<h1><i>My name is {fname} {lname}</i></h1>
-<p><i>Using template literals :-
-{`my name is ${fname} ${lname} and my lucky no is ${5+5}`}
-</i></p>
+<h1 style={heading}><i>{bleh}</i></h1>
+<Heading />
+<div className="row">
+    <div className="col-4">
+    <img src={img1} alt="random-image" />
+    </div>
+    <div className="col-4">
+    <img src={img2} alt="random-image" />
+    </div>
+    <div className="col-4">
+    <a href={links} target="_blank"><img src={img3} alt="random-image" /></a>
+    </div>
+</div>
 </>,document.getElementById('root'));
