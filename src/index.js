a2f5bb7eb89f6c76e0a6ca9be57f84be7b017132
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-06 13:35:26 +0530

    Using Props in arrays in React Js by Components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Card from './Card';
+import Sdata from './Sdata';
+console.log(Sdata[0]);
+
 
 ReactDOM.render(
     <>
-        <Card imgSrc="https://wallpapercave.com/wp/wp4056410.jpg" title="Original Series" seriesName="Grey's Anatomy" link="https://www.netflix.com/in/title/80990668?source=35" />
-        <Card imgSrc="https://wallpapercave.com/wp/wp4056410.jpg" title="Original Series" seriesName="himym" link="https://www.netflix.com/in/title/80990668?source=35" />
-        <Card imgSrc="https://wallpapercave.com/wp/wp4056410.jpg" title="Original Series" seriesName="Friends" link="https://www.netflix.com/in/title/80990668?source=35" />
-        <Card imgSrc="https://wallpapercave.com/wp/wp4056410.jpg" title="Original Series" seriesName="TVD" link="https://www.netflix.com/in/title/80990668?source=35" />
+        <Card imgSrc={Sdata[0].imgscr} title={Sdata[0].title} seriesName={Sdata[0].sname} link={Sdata[0].links} />
+        <Card imgSrc={Sdata[1].imgscr} title={Sdata[1].title} seriesName={Sdata[1].sname} link={Sdata[1].links} />
+        <Card imgSrc={Sdata[2].imgscr} title={Sdata[2].title} seriesName={Sdata[2].sname} link={Sdata[2].links} />
+        <Card imgSrc={Sdata[3].imgscr} title={Sdata[3].title} seriesName={Sdata[3].sname} link={Sdata[3].links} />
+        <Card imgSrc={Sdata[4].imgscr} title={Sdata[4].title} seriesName={Sdata[4].sname} link={Sdata[4].links} />
     </>,
     document.getElementById("root")
 );
