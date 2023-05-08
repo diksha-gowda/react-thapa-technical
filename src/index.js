5718817f3ce32c7855495a72a027f9d22e72c360
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-08 12:35:16 +0530

    Using map() in props using fat arrow function

--- src/index.js
+++ src/index.js
@@ -3,16 +3,13 @@ import ReactDOM from 'react-dom';
 import Card from './Card';
 import Result from './Result';
 
-// Using normal function
-function ncard(val) {
-    console.log(val);
-    return(<Card imgSrc={val.poster_path} title={val.title} seriesName={val.original_title} link={val.media_type} />);
-}
-
 ReactDOM.render(
     <>
         <h1 className='heading_style'><b><i>bleh bleh bleh</i></b></h1>
-       { Result.map(ncard)}
+        {/* // Using fat arrow function */}
+        {Result.map(function ncard(val){
+            return (<Card imgSrc={val.poster_path} title={val.title} seriesName={val.original_title} link={val.media_type} />);
+        })}
     </>,
     document.getElementById("root")
 );
