f4cf0c9c190a473e87cac091cb9245d3ba1118e9
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-09 17:54:33 +0530

    Handling Events in ReactJS - Changing background color using hooks (useState())

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import React from 'react'
-import SlotMatch from './SlotMach';
-
-const App = ()=>{
-  return(
-    <>
-    <SlotMatch />
-    </>
-  );
+import React, { useState } from 'react'
+const getRandomColor = ()=>{
+  let letters = '0123456789ABCDEF';
+  let color = '#';
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
 }
+
+const App = () => {
+  const purple = '#8e44ad';
+  const [bg, setBg] = useState(purple); // bg will fetch purple
+  const bgChange = ()=>{
+  let newbg = getRandomColor();
+  console.log(newbg);
+  setBg(newbg);
+}
+  return (
+    <>
+        <div style={{ backgroundColor: bg}}>
+          <button onClick={bgChange}>Click Me</button>
+        </div>
+    </>
+  )
+}
+
 export default App;
