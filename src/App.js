daf794e62e30577f72d51bcd7b063bc21b2faac1
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-09 12:11:11 +0530

    Using setInterval to create Digital Clock

--- src/App.js
+++ src/App.js
@@ -16,20 +16,27 @@ import React, { useState } from 'react'
 // }
 
 const App = ()=>{
-  let ntime = new Date().toLocaleTimeString();
-  const[time, utime] = useState(ntime);
-  // total 3 variables
-  // ntime - will give current time, time - will give time showed on doc, utime - will give updated time
-  const clickFunction = ()=>{
     let time = new Date().toLocaleTimeString();
-    utime(time);
-  }
+    const [ctime, utime] = useState(time);
+
+    const updateTime = ()=>{
+      time = new Date().toLocaleTimeString();
+      utime(time);
+    }
+
+// you can pass a callback function in setInterval()
+    setInterval(() => {
+      time = new Date().toLocaleTimeString();
+      utime(time);
+    }, 1000);
+    // OR
+    // setInterval(updateTime, 100);
+
   return(
     <>
-      <h1>{time}</h1>
-      <button onClick={clickFunction}><b><i>UPDATED</i></b></button>
-    </>
+      <h1><b>{time}</b></h1>
+      <button>Click</button>
+      </>
   );
 }
-
 export default App;
