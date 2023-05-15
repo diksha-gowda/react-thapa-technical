8593a18788a502aa8623ddafdb49e6743625bfd9
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-15 13:23:23 +0530

    Login Form Submit in React Js

--- src/App.js
+++ src/App.js
@@ -2,30 +2,50 @@ import React, {useState} from 'react'
 
 const App = () => {
   
+  // Name hook
   const [name, setName] = useState("");
   const [fullName, setFullName] = useState();
   const inputEvent = (event)=>{
     setName(event.target.value);
   }
-  const onSubmit = ()=>{
+
+  // Last Name hook
+  const [lastname, setLastName] = useState("");
+  const [fullLastname, setfullLastname] = useState();
+  const inputLastNameEvent = (event)=>{
+    setLastName(event.target.value);
+  }
+
+  // Email hook
+  const [email, setEmail] = useState("");
+  const [fullEmail, setFullEmail] = useState();
+  const inputEmail = (event)=>{
+    setEmail(event.target.value);
+  }
+  
+  const onSubmits = (event)=>{
+    event.preventDefault();   //prevents the default behaviour
     setFullName(name);
+    setFullEmail(email);
+    setfullLastname(lastname);
   }
   return (
     <>
+    <div className='main_div'>
+    <form onSubmit={onSubmits}>
       <div>
-        <h1>Hello, {fullName}</h1>
-        <input type='text' placeholder='Enter your name' onChange={inputEvent} value={name}/>
-        <button onClick={onSubmit}>Click 👍</button>
+        <h1>Hello, {fullName} {fullLastname}</h1>
+        <h1>Email:- {fullEmail}</h1>
+        <input type='text' placeholder='Enter your first name' onChange={inputEvent} value={name}/>
+        <input type='text' placeholder='Enter your last name' onChange={inputLastNameEvent} value={lastname}/>
+        <input type='email' placeholder='Enter your email' onChange={inputEmail} value={email} />
+        <button type='submit'>Submit 👍</button>
+      </div>
+      </form>
       </div>
     </>
   )
 }
 export default App;
 
-
-// // In React there are two types of components :- 
-//   Controlled and
-//   Uncontrolled Component
-
-// In a controlled component, form data is handled by a React Component,
-// the alternative is uncontrolled components, where form data is handled by the DOM itself.
+// using form tag by default redirects to some other page
