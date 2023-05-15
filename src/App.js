5d83fb307b08460fbbd3c5430b1849541ae97b00
Diksha Gowda <diksha.gowda@webenetic.com>
2023-05-15 17:19:19 +0530

    Handling Complex Multiple Input Form States in React JS - Sending data in useState in object form

--- src/App.js
+++ src/App.js
@@ -1,44 +1,58 @@
 import React, {useState} from 'react'
 
 const App = () => {
-  
-  // Name hook
-  const [name, setName] = useState("");
-  const [fullName, setFullName] = useState();
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: ""
+  });
   const inputEvent = (event)=>{
-    setName(event.target.value);
+    // event.target.attributeName
+    const value = event.target.value;
+    const name = event.target.name;
+    const placeholder = event.target.placeholder;
+    console.log("Value " + value);
+    console.log("Name " + name);
+    console.log("Placeholder " + placeholder);
+    setFullName((preValue)=>{
+      if( name === 'fName'){
+        return {
+          fname: value,
+          lname: preValue.lname,
+          email: preValue.email,
+        };
+      } else if( name === 'lName'){
+        return {
+          fname: preValue.fname,
+          lname: value,
+          email: preValue.email,
+        };
+      } else if( name === 'email'){
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email: value,
+        };
+      }
+    });
   }
 
-  // Last Name hook
-  const [lastname, setLastName] = useState("");
-  const [fullLastname, setfullLastname] = useState();
-  const inputLastNameEvent = (event)=>{
-    setLastName(event.target.value);
-  }
-
-  // Email hook
-  const [email, setEmail] = useState("");
-  const [fullEmail, setFullEmail] = useState();
-  const inputEmail = (event)=>{
-    setEmail(event.target.value);
-  }
-  
   const onSubmits = (event)=>{
-    event.preventDefault();   //prevents the default behaviour
-    setFullName(name);
-    setFullEmail(email);
-    setfullLastname(lastname);
+    event.preventDefault();
+    alert("Submit");
+    // window.location.reload(false);
   }
   return (
     <>
     <div className='main_div'>
     <form onSubmit={onSubmits}>
       <div>
-        <h1>Hello, {fullName} {fullLastname}</h1>
-        <h1>Email:- {fullEmail}</h1>
-        <input type='text' placeholder='Enter your first name' onChange={inputEvent} value={name}/>
-        <input type='text' placeholder='Enter your last name' onChange={inputLastNameEvent} value={lastname}/>
-        <input type='email' placeholder='Enter your email' onChange={inputEmail} value={email} />
+      Name: {fullName.fname} {fullName.lname}
+      <br></br>
+      Email: {fullName.email}
+        <input type='text' placeholder='Enter your first name' name='fName' onChange={inputEvent}/>
+        <input type='text' placeholder='Enter your last name' name='lName' onChange={inputEvent}/>
+        <input type='email' placeholder='Enter your email' name='email' onChange={inputEvent}/>
         <button type='submit'>Submit 👍</button>
       </div>
       </form>
