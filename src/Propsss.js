a763729941cccc09daaf9988ad630b5b33dd7b52
Diksha Gowda <diksha.gowda@webenetic.com>
2023-06-12 13:04:30 +0530

    passing variable in props

--- src/Propsss.js
+++ src/Propsss.js
@@ -4,9 +4,10 @@ const Car = (props)=>{
     return <><h1>I am a {props.brand}</h1></>
 };
 const Propsss = () => {
+    const carName = "Ford";
   return (
     <>
-    <Car brand="Ford" />
+    <Car brand={carName}/>
     </>
   )
 }
